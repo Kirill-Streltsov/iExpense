554b75218945422aed671eff8ba76d05711dfd87
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-29 21:09:40 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kirill Streltsov on 29.07.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
